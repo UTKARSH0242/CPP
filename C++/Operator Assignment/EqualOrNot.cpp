d9810aa6c2dee5b7c1820dab3236c2b2d0de4a89
Utkarsh Dubey <utkarshdubey0242@gmail.com>
2022-11-11 11:16:52 +0530

    updated

---
+++ C++/Operator Assignment/EqualOrNot.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;        
+
+int main()
+{
+    int n , m ;
+    cin >> n >> m ;
+    // if (n==m)
+    // {
+    //     cout <<"1";
+    //     /* code */
+    // }else
+    // {
+    //     cout <<"0";
+    //     /* code */
+    // }
+    cout << (n==m) ? 1:0;
+    return 0;
+    
+}
---
+++ C++/Operator Assignment/GirlsVsBoys.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    
+    int gradeAStudent, girls, boys;
+    boys = 17;
+    gradeAStudent = (80*45)/100;
+    girls = gradeAStudent - boys;
+    cout << girls << endl;
+    return 0;
+}
